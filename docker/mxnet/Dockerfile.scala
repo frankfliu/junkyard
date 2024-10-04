a18274703743cd383fbc49d1c4d807a972386370
Frank Liu <frankfliu2000@gmail.com>
2024-10-03 20:35:36 -0700

    remove mxnet related files

--- docker/mxnet/Dockerfile.scala
+++
@@ -1,9 +0,0 @@
-FROM mxnet-cpu
-
-RUN apt-get update \
-    && apt-get install -y --no-install-recommends \
-            openjdk-8-jdk-headless \
-            maven \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*
-
