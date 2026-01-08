67f737ce7df7d9914a29d29b0295b73de4b7d6d5
Frank Liu <frankfliu2000@gmail.com>
2026-01-07 17:25:44 -0800

    [lmbench] Add integration test for different server output format

--- lmbench/tests/lib_test.rs
+++ lmbench/tests/lib_test.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use httpmock::prelude::*;
+use indoc::indoc;
 use lmbench::args::Args;
 use lmbench::run;
 
@@ -21,4 +22,335 @@ async fn test_run_with_mock_server() {
 
     assert_eq!(stats.success_requests, 1);
     assert_eq!(stats.error_requests, 0);
+}
+
+#[tokio::test]
+async fn test_run_with_openai_mock() {
+    let server = MockServer::start();
+    let openai_mock = server.mock(|when, then| {
+        when.method(POST).path("/v1/chat/completions");
+        then.status(200)
+            .header("content-type", "application/json")
+            .body(
+                r#"{
+                    "choices": [
+                        {
+                            "message": {
+                                "content": "hello world"
+                            }
+                        }
+                    ]
+                }"#,
+            );
+    });
+
+    let args = Args::parse_from([
+        "lmbench",
+        "-d",
+        "hello",
+        "--tokens",
+        "-j",
+        "openai",
+        &server.url("/v1/chat/completions"),
+    ]);
+
+    let stats = run(args).await.unwrap();
+
+    openai_mock.assert();
+
+    assert_eq!(stats.success_requests, 1);
+    assert_eq!(stats.total_output_tokens, 2);
+}
+
+#[tokio::test]
+async fn test_run_with_openai_streaming_mock() {
+    let server = MockServer::start();
+    let openai_mock = server.mock(|when, then| {
+        when.method(POST).path("/v1/chat/completions");
+        then.status(200)
+            .header("content-type", "text/event-stream")
+            .body(indoc! { r#"
+                data: {"choices":[{"delta":{"content":"hello"}}]}
+
+                data: {"choices":[{"delta":{"content":" world"}}]}
+
+                data: [DONE]
+
+            "#});
+    });
+
+    let args = Args::parse_from([
+        "lmbench",
+        "-d",
+        "hello",
+        "--tokens",
+        "-j",
+        "openai",
+        &server.url("/v1/chat/completions"),
+    ]);
+
+    let stats = run(args).await.unwrap();
+
+    openai_mock.assert();
+
+    assert_eq!(stats.success_requests, 1);
+    assert_eq!(stats.total_output_tokens, 2);
+}
+
+#[tokio::test]
+async fn test_run_with_gemini_mock() {
+    let server = MockServer::start();
+    let gemini_mock = server.mock(|when, then| {
+        when.method(POST).path("/gemini:generateContent");
+        then.status(200)
+            .header("content-type", "application/json")
+            .body(
+                r#"{
+                    "candidates": [
+                        {
+                            "content": {
+                                "parts": [
+                                    {
+                                        "text": "hello world"
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }"#,
+            );
+    });
+
+    let args = Args::parse_from([
+        "lmbench",
+        "-d",
+        "hello",
+        "--tokens",
+        "-j",
+        "gemini",
+        &server.url("/gemini:generateContent"),
+    ]);
+
+    let stats = run(args).await.unwrap();
+
+    gemini_mock.assert();
+
+    assert_eq!(stats.success_requests, 1);
+    assert_eq!(stats.total_output_tokens, 2);
+}
+
+#[tokio::test]
+async fn test_run_with_gemini_streaming_mock() {
+    let server = MockServer::start();
+    let gemini_mock = server.mock(|when, then| {
+        when.method(POST).path("/gemini:streamGenerateContent");
+        then.status(200)
+            .header("content-type", "application/json")
+            .body(
+                r#"[{
+                    "candidates": [
+                        {
+                            "content": {
+                                "parts": [
+                                    {
+                                        "text": "hello world"
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }]"#,
+            );
+    });
+
+    let args = Args::parse_from([
+        "lmbench",
+        "-d",
+        "hello",
+        "--tokens",
+        "-j",
+        "gemini",
+        &server.url("/gemini:streamGenerateContent"),
+    ]);
+
+    let stats = run(args).await.unwrap();
+
+    gemini_mock.assert();
+
+    assert_eq!(stats.success_requests, 1);
+    assert_eq!(stats.total_output_tokens, 2);
+}
+
+#[tokio::test]
+async fn test_run_with_anthropic_mock() {
+    let server = MockServer::start();
+    let anthropic_mock = server.mock(|when, then| {
+        when.method(POST).path("/v1/messages");
+        then.status(200)
+            .header("content-type", "application/json")
+            .body(
+                r#"{
+                    "content": [
+                        {
+                            "text": "hello world"
+                        }
+                    ]
+                }"#,
+            );
+    });
+
+    let args = Args::parse_from([
+        "lmbench",
+        "-d",
+        "hello",
+        "--tokens",
+        "-j",
+        "anthropic",
+        &server.url("/v1/messages"),
+    ]);
+
+    let stats = run(args).await.unwrap();
+
+    anthropic_mock.assert();
+
+    assert_eq!(stats.success_requests, 1);
+    assert_eq!(stats.total_output_tokens, 2);
+}
+
+#[tokio::test]
+async fn test_run_with_anthropic_streaming_mock() {
+    let server = MockServer::start();
+    let anthropic_mock = server.mock(|when, then| {
+        when.method(POST).path("/v1/messages");
+        then.status(200)
+            .header("content-type", "text/event-stream")
+            .body(indoc! { r#"
+                data: {"delta":{"text":"hello"}}
+
+                data: {"delta":{"text":" world"}}
+
+                data: [DONE]
+
+            "#});
+    });
+
+    let args = Args::parse_from([
+        "lmbench",
+        "-d",
+        "hello",
+        "--tokens",
+        "-j",
+        "anthropic",
+        &server.url("/v1/messages"),
+    ]);
+
+    let stats = run(args).await.unwrap();
+
+    anthropic_mock.assert();
+
+    assert_eq!(stats.success_requests, 1);
+    assert_eq!(stats.total_output_tokens, 2);
+}
+
+#[tokio::test]
+async fn test_run_with_tgi_mock() {
+    let server = MockServer::start();
+    let tgi_mock = server.mock(|when, then| {
+        when.method(POST).path("/generate");
+        then.status(200)
+            .header("content-type", "application/json")
+            .body(
+                r#"{
+                    "generated_text": "hello world"
+                }"#,
+            );
+    });
+
+    let args = Args::parse_from([
+        "lmbench",
+        "-d",
+        "hello",
+        "--tokens",
+        "-j",
+        "TGI",
+        &server.url("/generate"),
+    ]);
+
+    let stats = run(args).await.unwrap();
+
+    tgi_mock.assert();
+
+    assert_eq!(stats.success_requests, 1);
+    assert_eq!(stats.total_output_tokens, 2);
+}
+
+#[tokio::test]
+async fn test_run_with_tgi_streaming_mock() {
+    let server = MockServer::start();
+    let body = indoc! { r#"
+        data: {"token":{"id":29889,"text":"Hello","logprob":-0.0028}}
+
+        data: {"token":{"id":29889,"text":" world","logprob":-0.0022}}
+
+    "#};
+    let tgi_mock = server.mock(|when, then| {
+        when.method(POST).path("/generate_stream");
+        then.status(200)
+            .header("content-type", "text/event-stream")
+            .body(body);
+    });
+
+    let args = Args::parse_from([
+        "lmbench",
+        "-d",
+        "hello",
+        "--tokens",
+        "-j",
+        "TGI",
+        &server.url("/generate_stream"),
+    ]);
+
+    let stats = run(args).await.unwrap();
+
+    tgi_mock.assert();
+
+    assert_eq!(stats.success_requests, 1);
+    assert_eq!(stats.total_output_tokens, 2);
+}
+
+#[tokio::test]
+async fn test_run_with_custom_jq() {
+    let server = MockServer::start();
+    let triton_mock = server.mock(|when, then| {
+        when.method(POST).path("/v2/models/ensemble/generate");
+        then.status(200)
+            .header("content-type", "application/json")
+            .body(
+                r#"{
+                  "model_name": "tensorrt_llm_bls",
+                  "model_version": "1",
+                  "outputs": [
+                    {"name": "text_output", "data": ["Hello world"]},
+                    {"name": "cum_log_probs", "data": [0.0]}
+                  ]
+                }"#,
+            );
+    });
+
+    let args = Args::parse_from([
+        "lmbench",
+        "-d",
+        "hello",
+        "--tokens",
+        "-j",
+        r#"$.outputs[?(@.name=="text_output")].data[0]"#,
+        &server.url("/v2/models/ensemble/generate"),
+    ]);
+
+    let stats = run(args).await.unwrap();
+
+    triton_mock.assert();
+
+    assert_eq!(stats.success_requests, 1);
+    assert_eq!(stats.total_output_tokens, 2);
 }
