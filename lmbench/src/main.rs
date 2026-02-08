306d69e360a9fd7f0def0bc8209aad2d539d1377
Frank Liu <frankfliu2000@gmail.com>
2026-02-08 12:06:03 -0800

    [lmbench] Return failure exit code if there is request error

--- lmbench/src/main.rs
+++ lmbench/src/main.rs
@@ -2,10 +2,11 @@ use clap::Parser;
 use lmbench::args::Args;
 use lmbench::run;
 use std::path::Path;
-use tracing_subscriber::{EnvFilter, Layer, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+use std::process::ExitCode;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer};
 
 #[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
+async fn main() -> ExitCode {
     let cli = Args::parse();
 
     let console_layer = fmt::layer()
@@ -13,8 +14,8 @@ async fn main() -> Result<(), anyhow::Error> {
         .with_filter(EnvFilter::from_default_env());
 
     let (file_layer, _guard) = if let Some(output_dir) = &cli.output {
-        tokio::fs::create_dir_all(output_dir).await?;
-        let file = std::fs::File::create(Path::new(output_dir).join("output.log"))?;
+        tokio::fs::create_dir_all(output_dir).await.unwrap();
+        let file = std::fs::File::create(Path::new(output_dir).join("output.log")).unwrap();
         let (non_blocking, guard) = tracing_appender::non_blocking(file);
         let layer = fmt::layer()
             .with_writer(non_blocking)
@@ -34,6 +35,10 @@ async fn main() -> Result<(), anyhow::Error> {
         .with(file_layer)
         .init();
 
-    let _ = run(cli).await?;
-    Ok(())
+    let stat = run(cli).await.unwrap();
+    if stat.error_requests > 0 {
+        ExitCode::FAILURE
+    } else {
+        ExitCode::SUCCESS
+    }
 }
